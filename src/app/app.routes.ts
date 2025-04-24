a21a87520160e8dc0c2b4d48dfcd2fc760c6a24c
fbansept <bansept.franck@gmail.com>
2025-04-24 12:03:15 +0200

    ajout tableau

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import {ConnexionComponent} from './pages/connexion/connexion.component';
 import {Page404Component} from './pages/page404/page404.component';
 import {EditProduitComponent} from './pages/edit-produit/edit-produit.component';
 import {connecteGuard} from './services/connecte.guard';
+import {ListeProduitComponent} from './pages/liste-produit/liste-produit.component';
 
 export const routes: Routes = [
   {path: "accueil", component: AccueilComponent, canActivate: [connecteGuard]},
   {path: "connexion", component: ConnexionComponent},
   {path: "ajout-produit", component: EditProduitComponent, canActivate: [connecteGuard]},
   {path: "modifier-produit/:id", component: EditProduitComponent, canActivate: [connecteGuard]},
+  {path: "liste-produit", component: ListeProduitComponent, canActivate: [connecteGuard]},
   {path: "", redirectTo: "accueil", pathMatch: "full"},
   {path: "**", component: Page404Component},
 ];
